188deb071a725b1ee3a1ae0ad172ce215677a8d0
nitink2306 <nitinkrishna2306@gmail.com>
2024-12-09 00:43:55 +0530

    finish reservation flow

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -36,7 +36,7 @@ export default async function Navigation() {
                 alt={session.user.name}
                 referrerPolicy="no-referrer"
               />
-              <span>Guest area</span>
+              <span>Account</span>
             </Link>
           ) : (
             <Link
